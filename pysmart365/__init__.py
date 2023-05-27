a087795effb11aed8a52881b905b5e2228af8794
informatic365 <informatic3650@gmail.com>
2023-05-27 19:07:59 +0200

    1

---
+++ pysmart365/__init__.py
@@ -0,0 +1,26 @@
+AUTHOR = 'Runkang'
+COPYRIGHT = '© Copyright 2023 Informatic365 - SmartSoft - MicroSoftware'
+
+import subprocess
+import platform
+
+def turn_off(time):
+    '''
+    Shut down pc directly without gui graphics
+    '''
+    if time is None or 0:
+        subprocess.run(['shutdown', '-s', '-t', '0'])
+    else:
+        subprocess.run(['shutdown', '-s', '-t', time])
+def restart(time):
+    if time is None or 0:
+        subprocess.run(['shutdown', '-r', '-t', '0'])
+    else:
+        subprocess.run(['shutdown', '-r', '-t', time])
+def restart_with_advancedmode(time):
+    if time is None or 0:
+        subprocess.run(['shutdown', '-r', '-o', '-t', '0'])
+    else:
+        subprocess.run(['shutdown', '-r', '-o', '-t', time])
+def turn_off_with_gui():
+    if platform.
