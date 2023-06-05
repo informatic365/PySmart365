71167235ee1bccc1cfeb973d56bba1673b1ddd37
informatic365 <informatic3650@gmail.com>
2023-06-05 17:15:30 +0200

    Update modules.py

--- pysmart365/modules.py
+++ pysmart365/modules.py
@@ -10,3 +10,9 @@ import json
 import jsonschema
 import firebase_admin
 from firebase_admin import db, auth, credentials, firestore, app_check, App, datetime
+import sys
+import configparser
+import argparse
+import flask
+from flask import Flask, views, app, config, globals, helpers, logging, cli, ctx, sessions, debughelpers, testing, wrappers, blueprints, scaffold
+from ttkbootstrap import *
